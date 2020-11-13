8b8ddd500d3adfab004dd811e64982ca35a58828
wutao <wutao@neuron.sg>
2020-11-13 15:00:54 +0800

    [WT]N/A:Add SingleLiveEvent.kt

---
+++ base/src/main/java/com/aisier/architecture/util/SingleLiveEvent.kt
@@ -0,0 +1,55 @@
+package com.aisier.architecture.util
+
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * <pre>
+ *     @author : wutao
+ *     e-mail : wutao@neuron.sg
+ *     time   : 2020/11/13
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val mPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
+        // Observe the internal MutableLiveData
+        super.observe(owner, object : Observer<T> {
+            override fun onChanged(t: T?) {
+                if (mPending.compareAndSet(true, false)) {
+                    observer.onChanged(t)
+                }
+            }
+        })
+    }
+
+    @MainThread
+    override fun setValue(t: T?) {
+        mPending.set(true)
+        super.setValue(t)
+    }
+
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+
+    companion object {
+        private val TAG = "SingleLiveEvent"
+    }
+}
