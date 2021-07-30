249b32143df82ffb7c3b1ffbc0ff97053d3099d2
wutao <wutao@neuron.sg>
2021-07-30 17:48:16 +0800

    [WT]N/A:增加 Activity Results API

--- app/src/main/java/com/aisier/ui/SecondActivity.kt
+++ app/src/main/java/com/aisier/ui/SecondActivity.kt
@@ -1,17 +1,28 @@
 package com.aisier.ui
 
+import android.Manifest
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.aisier.R
 import com.aisier.architecture.base.BaseActivity
 import com.aisier.architecture.util.toast
 import com.aisier.databinding.ActivitySecondBinding
+import com.aisier.util.TakePhotoLiveData
 import com.aisier.util.TimerShareLiveData
 import com.aisier.vm.ShareViewModel
 
 class SecondActivity : BaseActivity(R.layout.activity_second) {
 
     private val mBinding by viewBinding(ActivitySecondBinding::bind)
+
+    private lateinit var takePhotoLiveData: TakePhotoLiveData
+
+    private val requestPermission: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result: Boolean ->
+            toast("request permission $result")
+        }
 
     override fun init() {
         val shareViewModel = getAppViewModelProvider().get(ShareViewModel::class.java)
@@ -24,12 +35,25 @@ class SecondActivity : BaseActivity(R.layout.activity_second) {
             TimerShareLiveData.get().cancelTimer()
         }
 
+        takePhotoLiveData = TakePhotoLiveData(activityResultRegistry)
+        takePhotoLiveData.observeForever { bitmap ->
+            mBinding.imageView.setImageBitmap(bitmap)
+        }
+
+        mBinding.btTakePhoto.setOnClickListener {
+            takePhotoLiveData.takePhotoLauncher.launch(null)
+        }
+
         shareViewModel.msgLiveData.observe(this) {
             toast(it)
         }
 
         TimerShareLiveData.get().observe(this) {
             Log.i(TAG, "SecondActivity: $it")
+        }
+
+        mBinding.btRequestPermission.setOnClickListener {
+            requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 
---
+++ app/src/main/java/com/aisier/util/TakePhotoLiveData.kt
@@ -0,0 +1,28 @@
+package com.aisier.util
+
+import android.graphics.Bitmap
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.LiveData
+
+class TakePhotoLiveData(private val registry: ActivityResultRegistry) : LiveData<Bitmap>() {
+
+    lateinit var takePhotoLauncher: ActivityResultLauncher<Void?>
+
+    override fun onActive() {
+        super.onActive()
+        takePhotoLauncher = registry.register(
+            "key",
+            ActivityResultContracts.TakePicturePreview()
+        ) { result ->
+            value = result
+        }
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        takePhotoLauncher.unregister()
+    }
+
+}
